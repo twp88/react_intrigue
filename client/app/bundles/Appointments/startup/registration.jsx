d78dfb68821593733a3ba872bea7e83f1f761de0
Tom <tom.w.page@gmail.com>
2018-07-30 19:51:42 +0200

    Change file path registration

--- client/app/bundles/Appointments/startup/registration.jsx
+++ client/app/bundles/Appointments/startup/registration.jsx
@@ -1,6 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
-import Appointments from '../components/Appointments';
+import Appointments from '../components/appointments';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
